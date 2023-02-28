b963d639f8c10090ad0550668d3873ad44a60825
Julio Tambani <juliotambani@gmail.com>
2023-02-28 19:30:40 -0300

    Coparação do sorteio com o meu jogo.

--- index.js
+++ index.js
@@ -5,3 +5,17 @@ let firstGame = [10,25,33,45,55,59];
 for (let index=0;index<6;index+=1){
     lotteryNumbers.push(Math.ceil(Math.random()*60));
 }
+
+// Coparação do sorteio com o meu jogo
+
+let numberOfHits =0;
+let hits=[];
+
+for (let indexLottery=0; indexLottery<lotteryNumbers.length; indexLottery+=1){
+    for (let indexGame=0; indexGame<firstGame.length; indexGame+=1){
+        if (lotteryNumbers [indexLottery]=== firstGame[indexGame]){
+            numberOfHits+=1;
+            hits.push(lotteryNumbers[indexLottery]);
+        }
+    }
+}
